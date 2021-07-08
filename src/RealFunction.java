a31bff68777e41f6051d36b1c67b103eef4501cc
Ismail Sahbane <ismail.sahbane@epfl.ch>
2021-07-08 02:08:41 +0200

    ajout exp, log, sin et cos

--- src/RealFunction.java
+++ src/RealFunction.java
@@ -72,13 +72,14 @@ class Power implements RealFunction {
 
     @Override
     public RealFunction derivative() {
-        return new Product(new Constant(e), new Power(f, e - 1));
+        return new Product(f.derivative(),
+                new Product(new Constant(e), new Power(f, e - 1)));
     }
 
     @Override
     public String text() {
         if(e == 1) return f.text();
-        return f.text() + "^" + e;
+        return "(" + f.text() + ")" + "^" + e;
     }
 }
 
@@ -130,14 +131,118 @@ class Sum implements RealFunction {
     }
 }
 
+class Exponential implements RealFunction {
+    private final RealFunction f;
+
+    Exponential(RealFunction f) {
+        this.f = f;
+    }
+
+    @Override
+    public double y(double x) {
+        return Math.exp(f.y(x));
+    }
+
+    @Override
+    public RealFunction derivative() {
+        return new Product(f.derivative(), this);
+    }
+
+    @Override
+    public String text() {
+        return "exp(" + f.text() + ")";
+    }
+}
+
+class Logarithm implements RealFunction {
+    private final RealFunction f;
+
+    Logarithm(RealFunction f) {
+        this.f = f;
+    }
+
+    @Override
+    public double y(double x) {
+        return Math.log(f.y(x));
+    }
+
+    @Override
+    public RealFunction derivative() {
+        return new Product(f.derivative(), new Power(f, -1));
+    }
+
+    @Override
+    public String text() {
+        return "exp(" + f.text() + ")";
+    }
+}
+
+class Sine implements RealFunction {
+    private final RealFunction f;
+
+    Sine(RealFunction f) {
+        this.f = f;
+    }
+
+    @Override
+    public double y(double x) {
+        return Math.sin(f.y(x));
+    }
+
+    @Override
+    public RealFunction derivative() {
+        return new Product(f.derivative(), new Cosine(f));
+    }
+
+    @Override
+    public String text() {
+        return "sin(" + f.text() + ")";
+    }
+}
+
+class Cosine implements RealFunction {
+    private final RealFunction f;
+
+    Cosine(RealFunction f) {
+        this.f = f;
+    }
+
+    @Override
+    public double y(double x) {
+        return Math.cos(f.y(x));
+    }
+
+    @Override
+    public RealFunction derivative() {
+        return new Product( f.derivative(),
+                new Product(new Constant(-1), new Sine(f)));
+    }
+
+    @Override
+    public String text() {
+        return "cos(" + f.text() + ")";
+    }
+}
+
 class Main {
     public static void main(String[] args){
         RealFunction polynom = new Sum(
                 new Product(new Constant(2), new Power(new Identity(), 2)), // 2x^2
                 new Identity() //x
         );
-        System.out.println(polynom.text());
-        RealFunction derivative = polynom.derivative(); //should be 4x + 1
+        RealFunction tangeant = new Product(new Sine(new Identity()),
+                new Power(new Cosine(new Identity()), -1));
+        RealFunction bordel = new Power(
+                new Sum(
+                        new Product(new Constant(2), new Power(new Identity(), 4)),
+                        new Exponential(new Product(new Constant(-1),
+                                new Sum(new Product(new Constant(4), new Identity()), new Constant(3))))),
+                0.6d);
+
+        RealFunction function = bordel;
+        System.out.println(function.text());
+        RealFunction derivative = function.derivative(); //should be 4x + 1
         System.out.println(derivative.text());
+        for(int i = 0; i < 10; ++i) System.out.println(derivative.y(i) +", ");
     }
 }
